5dce02c9b3dc2ed3838a7fbac90a59d30030e122
AgathaLice <miguelfabrisd@gmail.com>
2025-06-03 23:26:51 -0300

    Mudanças e AVISO EM VIEW

--- View/Telas/TelaMenu.py
+++ View/Telas/TelaMenu.py
@@ -2,3 +2,5 @@ import tkinter as tk #TODO Lembra que isso vai ser o TkCustom depois
 
 class TelaMenu():
     
+    def desenha():
+        pass
--- View/View.py
+++ View/View.py
@@ -4,6 +4,8 @@ from Telas import TelaMenu
 from Telas import TelaTutorial
 from Telas import TelaVisualizacao
 
+#! VOCÊ NÃO CONSEGUE FAZER CADA TELA EM UM FILE DIFERENTE
+
 from Controller import Controller
 
 import tkinter as tk #TODO Lembra que isso vai ser o TkCustom depois
@@ -14,6 +16,7 @@ class View():
         self.root = tk.Tk()
         
         self.controller = Controller(self)
+        
         self.telaMenu = TelaMenu(self)
         self.telaInsercao = TelaInsercao(self)
         self.telaListagem = TelaListagem(self)
@@ -34,4 +37,39 @@ class View():
     def inicia(self):
         #* Define a janela com as telas, instanciando e desenhando todas.
         #! Chama cada tela, da última a primeira, dando RAISE na TELA DE MENU.
+        
+        TelaMenu.menu = tk.Frame(self.root)
+        TelaMenu.menu.grid(row = 12,
+                  column = 12,
+                  rowspan = 12,
+                  columnspan = 12,
+                  sticky = tk.W+tk.E+tk.N+tk.S)
+        
+        TelaInsercao.insercao = tk.Frame(self.root)
+        TelaInsercao.insercao.grid(row = 12,
+                  column = 12,
+                  rowspan = 12,
+                  columnspan = 12,
+                  sticky = tk.W+tk.E+tk.N+tk.S)
+        
+        TelaListagem.listagem = tk.Frame(self.root)
+        TelaListagem.listagem.grid(row = 12,
+                  column = 12,
+                  rowspan = 12,
+                  columnspan = 12,
+                  sticky = tk.W+tk.E+tk.N+tk.S)
+        
+        TelaVisualizacao.visualizacao = tk.Frame(self.root)
+        TelaVisualizacao.visualizacao.grid(row = 12,
+                  column = 12,
+                  rowspan = 12,
+                  columnspan = 12,
+                  sticky = tk.W+tk.E+tk.N+tk.S)
+        
+        TelaTutorial.turorial = tk.Frame(self.root)
+        TelaTutorial.turorial.grid(row = 12,
+                  column = 12,
+                  rowspan = 12,
+                  columnspan = 12,
+                  sticky = tk.W+tk.E+tk.N+tk.S)
         return None
